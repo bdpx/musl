da2d3343be24f0fd919935b71a3a60fee0e7bb29
Dmitry Buvailo <moxoron@gmail.com>
2023-05-28 22:48:59 +0300

    [Postrisc] postrisc fixes

--- crt/crt1.c
+++ crt/crt1.c
@@ -1,6 +1,11 @@
 #include <features.h>
 #include "libc.h"
 
+// defines for c-source crt_arch.h
+#define START_FUNC _start
+#define START_FUNC_C _start_c
+
+// defines for inline-asm crt_arch.h
 #define START "_start"
 
 #include "crt_arch.h"
@@ -11,7 +16,7 @@ weak void _fini();
 int __libc_start_main(int (*)(), int, char **,
 	void (*)(), void(*)(), void(*)());
 
-hidden void _start_c(long *p)
+hidden void _start_c(size_t *p, size_t const * /*dynamic*/) // should be identical to _dlstart_c by prototype
 {
 	int argc = p[0];
 	char **argv = (void *)(p+1);
